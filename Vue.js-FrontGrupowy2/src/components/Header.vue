f58bfdac3b2a5b6e8c8611b4fe917b4e0e981b5b
Dawid S <dawidstaszewski13@gmail.com>
2024-05-04 23:03:09 +0200

    Init

---
+++ Vue.js-FrontGrupowy2/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+          <div class=" w-full flex justify-center relative _mainTop">
+            <div
+                class="
+                    fixed
+                    w-284
+                    bg-black
+                    flex
+                    text-white
+                    justify-between
+                    items-center
+                    top-0
+                    left-1/2
+                    -translate-x-1/2
+                    flex-col
+                    rounded-xl
+                    z-10
+                    _mainMenu
+                    py-6
+
+                "
+            >
+                <div class="_top flex w-full justify-between h-15 px-4 overflow-hidden">
+                    <div class="flex items-center gap-4 text-xl">
+                        <div>AAAAAAAAAAAAAA</div>
+                        <div>AAAAAAAAAAAAAA</div>
+                        <div>AAAAAAAAAAAAAA</div>
+                        <div>AAAAAAAAAAAAAA</div>
+                        <div>AAAAAAAAAAAAAA</div>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-0 left-50 h-50 w-50 absolute"></div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
