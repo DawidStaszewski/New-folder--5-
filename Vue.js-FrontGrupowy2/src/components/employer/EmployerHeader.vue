e0e2bf6141f484e01c21d2b45b3c7f9156d0ab15
PublicVoidToString <kml.pieper@gmail.com>
2024-12-14 21:51:36 +0100

    Fixed naming in Employer Sidebar and Header

--- Vue.js-FrontGrupowy2/src/components/employer/EmployerHeader.vue
+++ Vue.js-FrontGrupowy2/src/components/employer/EmployerHeader.vue
@@ -4,8 +4,8 @@
     <!-- User Information -->
     <div class="flex items-center space-x-4">
       <div>
-        <p class="text-lg font-semibold">Jan Kowalski</p>
-        <p class="text-sm text-gray-400">Student</p>
+        <p class="text-lg font-semibold">{{ employerName }}</p>
+        <p class="text-sm text-gray-400">Pracodawca</p>
       </div>
     </div>
   </div>
@@ -13,9 +13,41 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import api from '@/services/api'
+
 export default {
   name: "EmployerHeader",
-};
+  setup() {
+    // Deklaracja zmiennej reaktywnej
+    const employerName = ref('')
+
+    // Funkcja pobierająca dane employera
+    const fetchEmployerName = async () => {
+      try {
+        const response = await api.getUser();
+        console.log('Response data:', response.data)
+
+        // Aktualizacja reaktywnej zmiennej
+        employerName.value = response.data.user?.data?.name || response.data.user?.email || 'Nieznane imię'
+        console.log('Employer name:', employerName.value)
+      } catch (err) {
+        console.error('Błąd podczas pobierania danych użytkownika', err)
+      }
+    }
+
+    // Wywołanie funkcji podczas montowania komponentu
+    onMounted(() => {
+      fetchEmployerName()
+    })
+
+    // Eksport zmiennych i funkcji dla template
+    return {
+      employerName,
+    }
+  }
+}
 </script>
 
 <style scoped>
